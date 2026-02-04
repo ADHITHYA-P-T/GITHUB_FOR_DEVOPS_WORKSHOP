302ef27833e1b6b42d9d0b48571277d3f318fc31
ADHITHYA-P-T <adhithyagirishpotofficial@gmail.com>
2026-02-04 23:35:54 +0530

    Fixed Pylint warnings and added module/function docstrings

--- demo.py
+++ demo.py
@@ -1,6 +1,13 @@
+"""
+demo.py module
+This module contains a demo function.
+"""
+
 def my_function():
-    
-    a=5
-    b=6
+    """
+    This function demonstrates returning a value.
+    """
+    a = 5
     return a
+
 my_function()
